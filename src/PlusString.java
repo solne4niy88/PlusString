c9a4addd2a73f136e3d329de24f1f5b715bffed4
solne4niy <solne4niy88@yandex.ru>
2024-07-03 07:43:57 +0300

    created набросок

---
+++ src/PlusString.java
@@ -0,0 +1,10 @@
+
+public class PlusString {
+
+    private String scan;
+    String text = scan;
+    private String plusString() {
+        text = text.split("\n");
+
+    }
+}
---
+++ src/Scan.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Scan {
+    String words;
+    private String inputWords() {
+        words = String.valueOf(new Scanner(System.in));
+        return words;
+    }
+    public
+}
